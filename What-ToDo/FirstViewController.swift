1d5a4506b7201e8411ec6b1725dc6214f63fb984
soumyajitz <2014soumyajit@gmail.com>
2016-01-31 18:39:57 -0600

    A Simple To Do List App
    ToDoList App with Permanent Storage capabilities user NSUserDefaults,
    Swipe Left to delete items, keyboard user control facilities

--- What-ToDo/FirstViewController.swift
+++ What-ToDo/FirstViewController.swift
@@ -8,16 +8,51 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+// Create a Global toDoList variable
+var toDoList = [String]()
+
+class FirstViewController: UIViewController ,UITableViewDelegate{
+    
+
+    @IBOutlet var toDoListTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Check to see if toDoList exists or not
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+        
+        toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+
+    let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+    cell.textLabel?.text = toDoList[indexPath.row]
+    return cell
+    }
+    // To update the table values, we can use the viewDidAppear method
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+    // Code to swipe left and delete from toDoList
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            toDoListTable.reloadData()
+        }
+        
     }
 
 
--- What-ToDo/SecondViewController.swift
+++ What-ToDo/SecondViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!)
+        item.text = ""
+        // Update NSUserDefaults
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.item.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField :UITextField!) -> Bool{
+        item.resignFirstResponder()
+        return true
+    }
 
 }
 
